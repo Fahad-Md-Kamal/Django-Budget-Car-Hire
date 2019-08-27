e8e31eaf9ecf7c8e2cbdb102655c7b07dd7678e4
Fahad Md Kamal <faahad.hossain@gmail.com>
2019-08-27 19:57:34 +0600

    Fixed Bug: force_insert()

--- Budget-car-hire/users/forms.py
+++ Budget-car-hire/users/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
-
+from . import models
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
@@ -10,3 +10,17 @@ class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
+
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ('username', 'email')
+
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = models.profile
+        fields = ['house','road','city','contact','image']
--- Budget-car-hire/users/models.py
+++ Budget-car-hire/users/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from PIL import Image
 
 class profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -12,3 +12,13 @@ class profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username} profile'
+
+    def save(self, *args, **kwargs):
+        super(profile, self).save(*args, **kwargs)
+
+        img = Image.open(self.image.path)
+        
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
