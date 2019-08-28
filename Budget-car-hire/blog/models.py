86372c40e0397b21e689f4dab2077f4bcac253be
Fahad Md Kamal <faahad.hossain@gmail.com>
2019-08-28 11:27:14 +0600

    Showing comments for each Blog

--- Budget-car-hire/blog/models.py
+++ Budget-car-hire/blog/models.py
@@ -22,6 +22,7 @@ class Comment(models.Model):
     user = models.ForeignKey(User, on_delete= models.CASCADE, related_name="user_comment")
     article = models.ForeignKey(Article, on_delete = models.CASCADE, related_name="comments")
     comment = models.TextField(max_length= 256)
+    comment_date = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.comment
