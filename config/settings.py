c43cbbf691251209d114139fbffc92303e5a7e70
fahad-md-kamal <faahad.hossain@gmail.com>
2021-09-26 20:40:02 +0600

    static folder modified

--- config/settings.py
+++ config/settings.py
@@ -106,7 +106,7 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = "/static/"
+STATIC_ROOT = "static"
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/media')
